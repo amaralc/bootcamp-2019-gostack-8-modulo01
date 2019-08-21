ba96994b31e9922cbf04c470e84b7508e4f4526d
amaralc <amaral.calil@gmail.com>
2019-08-20 23:30:09 -0300

    CRUD: Created methods get, post, put and delete

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 // Query params = ?teste=1
 // Ex: > const nome = req.query.nome;
 
@@ -10,14 +12,47 @@ const server = express();
 
 // Request body = {"name": "Calil", "email": "calil@email"}
 
+// CRUD - Create, Read, Update, Delete
+
 
 const users = ['Diego', 'Cláudio', 'Victor'];
 
-//localhost:3000/teste
+// retorna usuários
+
+server.get('/users', (req, res) => {
+  return res.json(users);
+})
+
+//retorna um usuário
 
 server.get('/users/:index', (req, res) => {
   const { index } = req.params;
   return res.json(users[index]);
 });
+
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+})
+
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+})
+
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.send();
+})
 
 server.listen(3000);
