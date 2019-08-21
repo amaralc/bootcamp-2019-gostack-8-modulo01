26802beb6739f33349486bbe3ccfa1ffa25a3dc5
amaralc <amaral.calil@gmail.com>
2019-08-20 22:27:26 -0300

    Query & Route params: added route params

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ const express = require('express');
 const server = express();
 
 // Query params = ?teste=1
+// Ex: > const nome = req.query.nome;
+
 // Route params = /users/1
+// Ex: > const id = req.params.id;
+
 // Request body = {"name": "Calil", "email": "calil@email"}
 
 
 //localhost:3000/teste
 
-server.get('/teste', (req, res) => {
-  const nome = req.query.nome;
-  const email = req.query.email;
-  return res.json({ message: `Hello ${nome}, seu e- mail é ${email}` });
+server.get('/user/:id', (req, res) => {
+  const id = req.params.id;
+  return res.json({ message: `Buscando o usuário ${id}` });
 });
 
 server.listen(3000);
